1375f00afc8c319387b3dae80d86c99ec8ad1303
Yulia <Yulia159V@gmail.com>
2021-03-15 22:53:35 +0300

    feat: add dicision

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let str1 = str.split(''),
+        stack = [],
+        open = ['{', '(', '[',],
+        close = ['}', ')', ']',],
+        closeIndex,
+        openIndex;
+
+    for (let i = 0, len = str1.length; i < len; i++) {
+       openIndex = open.indexOf(str1[i]);
+       if (openIndex !== -1) {
+           stack.push(openIndex);
+           continue;
+       }
+
+       closeIndex = close.indexOf(str1[i]);
+       if (closeIndex !== -1) {
+           openIndex = stack.pop();
+           if (closeIndex !== openIndex) {
+               return false;
+           }
+       }
+    }
+    if (stack.length !== 0) {
+        return false;
+    }
+
+    return true;
 }
